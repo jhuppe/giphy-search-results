5be895329c4291bf3c62483815c65068846aa14e
jerhup <jeremy.huppe@extendhealth.com>
2018-09-07 10:26:13 -0600

    Set up project.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Gif Searcher</h1>
+    <input v-model="searchTerm" type="text">
+    <button class="button" @click=getGifs()>Search</button>
+    <div class="gif-container">
+      <img v-for="gif in gifs" :src="gif" :key="gif.id">
+    </div> 
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      searchTerm: "",
+      gifs: []
+    };
+  }, 
+  methods: {
+    getGifs() {
+      let apiKey = "R9h97hQRlnxC9yqa8nDHnewVl8kpx2CL";
+      let searchEndPoint = "https://api.giphy.com/v1/gifs/search?";
+      let limit = 5;
+      let url = `${searchEndPoint}&api_key=${apiKey}&q=${
+        this.searchTerm
+        }&limit=${limit}`;
+
+        fetch(url)
+          .then(response => {
+            return response.json();
+          })
+          .then(json => {
+          this.buildGifs(json);
+        })
+        .catch(err => console.log(err));
+    },
+    buildGifs(json) {
+      this.gifs = json.data.map(gif => gif.id).map(gifId => {
+        return `https://media.giphy.com/media/${gifId}/giphy.gif`;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +53,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input {
+  padding: 5px;
+  margin-bottom: 20px;
+}
+.button {
+  background-color: rgb(0,172,0);
+  color: white;
+  padding: 5px 20px;
+  border: none;
+  display: block;
+  margin: 0 auto;
+}
+.button:hover {
+  background-color: rgb(0,148,0);
+}
+.gif-container {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
 }
 </style>
